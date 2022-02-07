c67261dc28f267fda585a2a266cd7c503b42f35d
vkfkstptkd1 <vkfkstptkd1@naver.com>
2022-02-07 21:23:31 +0900

    first

---
+++ LearnReactNavigation/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+function App(){
+  return <NavigationContainer>{/*내비게이션 설정*/}</NavigationContainer>
+}
+
+export default App;
