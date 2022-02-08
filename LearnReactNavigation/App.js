d69796d4f46c85be24b4933dd5a89bd2176ae649
vkfkstptkd1 <vkfkstptkd1@naver.com>
2022-02-08 17:05:08 +0900

    네비게이션 기본사용법_헤더커스터마이징_detailscreen 컴포 useeffect에서 설정

--- LearnReactNavigation/App.js
+++ LearnReactNavigation/App.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
 
+const Stack = createNativeStackNavigator();
+
 function App(){
   return (<NavigationContainer>
     <Stack.Navigator initialRouteName="Home">
@@ -11,12 +13,23 @@ function App(){
                     component={HomeScreen}  
                     options={{
                       title: '홈홈홈',
+                    //header 블록 스타일
+                      headerStyle:{
+                        backgroundColor: '#000000',
+                      },
+                      //header의 텍스트, 버튼들 색상
+                      headerTintColor: '#ffffff',
+                      //타이틀 텍스트의 스타일
+                      headerTitleStyle: {
+                        fontweight: 'bold',
+                        fontSize: 20,
+                      },
                     }}
       />
       <Stack.Screen name="Detail" 
                     component={DetailScreen} 
-                    options={({route})=>({                  //app에서 stack option으로 나타내려면 반환함수 사용.
-                      title: `상세정보 - ${route.params.id}`,
+                    options={({route})=>({                   //app에서 stack option으로 나타내려면 반환함수 사용.
+                      title: `상세정보 - ${route.params.id}`, //반환값이 객체타입이라면 객체를 소괄호로 감싸줘야함. =>{ return {title: `상세정보 - ${route.params.id}`}} 과 같은 의미
                     })}
       />
     </Stack.Navigator>
--- LearnReactNavigation/screens/HomeScreen.js
+++ LearnReactNavigation/screens/HomeScreen.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View, Button} from 'react-native';
 
 function HomeScreen({navigation}){
+    useEffect(() => {
+        navigation.setOptions({title: '홈홈홈'}); //useeffect를 통해 설정한 내비게이션 option은 app컴포넌트에서 props를 통해 설정한 option을 덮어씀.
+    },[navigation]);                             //ES 규칙상 useEffect 내부에서 사용하는 값을 꼭 deps(두번째 인자에 넣는 배열)에 넣어야 함.
     return(
         <View>
             <Button title="DETAIL 열기1" 
